d7d52c49d2a65c0c8f690fe7aa2eebf07a493792
Paula0M <paulamirazf@gmail.com>
2025-11-11 14:01:43 +0100

    Mejorando ProjectSection

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,28 +3,22 @@ import Card from './Card';
 
 const projects = [
   {
-    title: 'React Space',
+    title: 'Proyectos React',
     description:
       'Handy tool to display stats about the launches taking place in the SpaceX program',
     imageSrc: 'https://via.placeholder.com/400x220/A67B5B/FFFFFF?text=React+Space', // Imágenes con tu color principal
   },
   {
-    title: 'React Jokes',
+    title: 'Proyectos UX/UI',
     description:
       'A daily dose of jokes on your screen, powered by React and a third-party API',
     imageSrc: 'https://via.placeholder.com/400x220/BBAB8C/000000?text=React+Jokes', // Imágenes con tus colores
   },
   {
-    title: 'Photo Gallery',
+    title: 'Proyectos Wordpress',
     description:
       'A simple React photo gallery application fetching images from Unsplash API',
     imageSrc: 'https://via.placeholder.com/400x220/DED0B6/000000?text=Photo+Gallery',
-  },
-  {
-    title: 'Event Planner',
-    description:
-      'An event planning app built with React that allows users to create and manage events',
-    imageSrc: 'https://via.placeholder.com/400x220/FAEED1/A67B5B?text=Event+Planner',
   },
 ];
 
@@ -33,13 +27,14 @@ const ProjectsSection = () => {
     <section
       id="projects-section"
       style={{
-        minHeight: '100vh',
+        minHeight: '70vh',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         padding: 'calc(var(--spacing-unit) * 4) var(--spacing-unit)',
         backgroundColor: 'var(--color-accent)', // Fondo de acento
         color: 'var(--color-text-dark)',
+        alignContent: 'center',
       }}
     >
       <h2 style={{
