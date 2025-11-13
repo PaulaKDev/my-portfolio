fc361df171f87c4b7e578186741e3fcbe5b38863
Paula0M <paulamirazf@gmail.com>
2025-11-13 11:23:31 +0100

    Wordpress created and exported

---
+++ src/pages/WordPressProjects.jsx
@@ -0,0 +1,12 @@
+// src/pages/WordPressProjects.jsx
+import React from 'react';
+
+function WordPressProjects() {
+  return (
+    <div>
+      <h1>Proyectos WordPress</h1>
+    </div>
+  );
+}
+
+export default WordPressProjects;
