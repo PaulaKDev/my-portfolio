0554320e3dbb7944458fb2c80499beb74eb6d980
Paula0M <paulamirazf@gmail.com>
2025-11-13 12:26:19 +0100

    UXUIProject Card creada

--- src/pages/UXUIProjects.jsx
+++ src/pages/UXUIProjects.jsx
@@ -5,6 +5,11 @@ function UXUIProjects() {
   return (
     <div>
       <h1>Proyectos UX/UI</h1>
+      <div className="projects-grid">
+        {projects.uxui.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+        ))}
+      </div>
     </div>
   );
 }
