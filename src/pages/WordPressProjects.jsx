dfe9c854260d14871b63ef6a89837225f11431f1
Paula0M <paulamirazf@gmail.com>
2025-11-13 12:27:14 +0100

    WordpressProject card creada

--- src/pages/WordPressProjects.jsx
+++ src/pages/WordPressProjects.jsx
@@ -5,6 +5,11 @@ function WordPressProjects() {
   return (
     <div>
       <h1>Proyectos WordPress</h1>
+        <div className="projects-grid">
+        {projects.wordpress.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+        ))}
+      </div>
     </div>
   );
 }
