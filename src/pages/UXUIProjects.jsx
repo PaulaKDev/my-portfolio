db7d87e1406e9355838845a2f05515fa772d1673
Paula0M <paulamirazf@gmail.com>
2025-11-13 13:04:55 +0100

    importado project,json y projectCard en UIProjects

--- src/pages/UXUIProjects.jsx
+++ src/pages/UXUIProjects.jsx
@@ -1,9 +1,11 @@
 // src/pages/UXUIProjects.jsx
 import React from 'react';
+import projects from '../data/projects';
+import ProjectCard from '../components/ProjectCard';
 
 function UXUIProjects() {
   return (
-    <div>
+    <div style={{ padding: '2rem' }}>
       <h1>Proyectos UX/UI</h1>
       <div className="projects-grid">
         {projects.uxui.map((project, index) => (
