40460ab5d62acfafef05ee592d479cefdf9ec99b
Paula0M <paulamirazf@gmail.com>
2025-11-13 11:22:38 +0100

    UX Project created and exported

---
+++ src/pages/UXUIProjects.jsx
@@ -0,0 +1,12 @@
+// src/pages/UXUIProjects.jsx
+import React from 'react';
+
+function UXUIProjects() {
+  return (
+    <div>
+      <h1>Proyectos UX/UI</h1>
+    </div>
+  );
+}
+
+export default UXUIProjects;
