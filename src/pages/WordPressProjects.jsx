a1deaf29f7e36cbc3fcb9cb1c3f58c2a6a7cbaff
Paula0M <paulamirazf@gmail.com>
2025-11-13 13:06:07 +0100

    importado projects.json y ProjectCard a WordPressProjects

--- src/pages/WordPressProjects.jsx
+++ src/pages/WordPressProjects.jsx
@@ -1,9 +1,11 @@
 // src/pages/WordPressProjects.jsx
 import React from 'react';
+import projects from '../data/projects';
+import ProjectCard from '../components/ProjectCard';
 
 function WordPressProjects() {
   return (
-    <div>
+    <div style={{ padding: '2rem' }}>
       <h1>Proyectos WordPress</h1>
         <div className="projects-grid">
         {projects.wordpress.map((project, index) => (
