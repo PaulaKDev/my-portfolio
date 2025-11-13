e9a36920623669f996a4d71044d5b9c1627a751d
Paula0M <paulamirazf@gmail.com>
2025-11-13 13:01:16 +0100

    Importado projects.json en ReactProjects.jsx

--- src/pages/ReactProjects.jsx
+++ src/pages/ReactProjects.jsx
@@ -1,10 +1,11 @@
 // src/pages/ReactProjects.jsx
 import React from 'react';
+import projects from '../data/projects';
 import ProjectCard from '../components/ProjectCard';
 
 function ReactProjects() {
   return (
-    <div>
+    <div style={{ padding: '2rem' }}>
       <h1>Proyectos React</h1>
       <div className="projects-grid">
         {projects.react.map((project, index) => (
